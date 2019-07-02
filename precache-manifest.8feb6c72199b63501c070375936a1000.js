629dc66183ce6fcbb48c7839babd48dec769e46f
Sueldo Sales <sueldosales@gmail.com>
2019-07-02 10:13:14 -0300

    [UPD] React build

---
+++ precache-manifest.8feb6c72199b63501c070375936a1000.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b692a9c9b1248380de4ac6794e11894f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a7473cec2260d2c24e7",
+    "url": "/static/css/2.f53ec874.chunk.css"
+  },
+  {
+    "revision": "e58f612617b56290522f",
+    "url": "/static/css/main.aa21fa8f.chunk.css"
+  },
+  {
+    "revision": "9a7473cec2260d2c24e7",
+    "url": "/static/js/2.2d717d87.chunk.js"
+  },
+  {
+    "revision": "e58f612617b56290522f",
+    "url": "/static/js/main.e553a332.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
