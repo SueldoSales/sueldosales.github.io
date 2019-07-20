5bc056770a1a6fa76eb966bd11cb32a3408cc14e
Sueldo Sales <sueldosales@gmail.com>
2019-07-20 09:33:20 -0300

    Updates

---
+++ precache-manifest.d17d22a6bc27d1af66060493b303c234.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bbd3b0d2edb1da3650a17765628d416",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e1f29658a5690ee7132",
+    "url": "/static/css/2.e07e82fc.chunk.css"
+  },
+  {
+    "revision": "c3c763b8ab18b5dc1708",
+    "url": "/static/css/main.aa21fa8f.chunk.css"
+  },
+  {
+    "revision": "8e1f29658a5690ee7132",
+    "url": "/static/js/2.2ce94078.chunk.js"
+  },
+  {
+    "revision": "c3c763b8ab18b5dc1708",
+    "url": "/static/js/main.270fabc1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
