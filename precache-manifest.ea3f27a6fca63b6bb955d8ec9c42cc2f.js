b24e7840f529f99a5be4eb1c46ec7523e7d36550
Sueldo Sales <sueldosales@gmail.com>
2020-01-07 16:53:22 -0300

    [UPD] 2020

---
+++ precache-manifest.ea3f27a6fca63b6bb955d8ec9c42cc2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c9eb7e253630a3bcd5bdb1f0d879cb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4d306c8e92784b99983",
+    "url": "/static/css/main.22731d79.chunk.css"
+  },
+  {
+    "revision": "0301a86e821a127d69a8",
+    "url": "/static/js/2.516e7a1f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.516e7a1f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4d306c8e92784b99983",
+    "url": "/static/js/main.910beec1.chunk.js"
+  },
+  {
+    "revision": "2893486346305e5caa98",
+    "url": "/static/js/runtime-main.d9d58f53.js"
+  }
+]);
